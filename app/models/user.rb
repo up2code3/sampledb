50a5d05820fd394d172441d713c3803352618420
Tristan <the_position3030@yahoo.com>
2025-08-19 11:05:52 -0400

    format and style cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    has_many :tracks
-    has_many :comments
-    # TODO gameplan user.delete, what happens to their entries and comments.
+  has_many :tracks
+  has_many :comments
+  # TODO gameplan user.delete, what happens to their entries and comments.
 
-    validates :email, presence: true, uniqueness: true
-    validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
