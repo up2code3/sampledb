334586c46174daed6970b5fe1327ff7ed255a85f
Tristan <the_position3030@yahoo.com>
2025-09-19 14:27:42 -0400

    Track Artist title and if else none samples used or by

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :artists, only: [ :index, :show ]
   resources :tracks,  only: [ :index, :show ]
 
-  get "/dashboard", to: "dashboard#index"
+  get "/dash", to: "dashboard#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
